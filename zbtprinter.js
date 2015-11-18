8cfbf9e3cf6d23347944d84057f75e6675a33e7b
CETIN TOZKOPARAN <cetin.tozkoparan@consultant.turkcell.com.tr>
2015-11-18 17:29:45 +0200

    providing dynamic mac address

--- zbtprinter.js
+++ zbtprinter.js
@@ -12,7 +12,7 @@
     }
 
     ZebraBluetoothPrinter.prototype.esegui = function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'print', ["ciccio"]);
+        exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'print', ["ciccio", "AC:3F:A4:52:73:C4"]);
     };
     var bluetoothPrinter = new ZebraBluetoothPrinter();
     module.exports = bluetoothPrinter;
